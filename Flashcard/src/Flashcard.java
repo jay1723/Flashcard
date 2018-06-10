70344c5b05dbb5af3fded6279fb47c66594bc54b
Jay <jay@Jays-MacBook-Pro.local>
2018-06-09 23:09:55 -0400

    Added Flashcard class

--- Flashcard/src/Flashcard.java
+++ Flashcard/src/Flashcard.java
@@ -1,4 +1,38 @@
 
 public class Flashcard {
-
+	private String title;
+	private String description;
+	private boolean correct;
+	
+	// TODO: Implement Optional third argument
+	public Flashcard(String title, String desc, boolean correct) {
+		this.title = title;
+		this.description = desc;
+		this.correct = correct;
+	}
+	
+	// Getters and Setters
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public void setTitle(String val) {
+		this.title = val;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public void setDescription(String val) {
+		this.description = val;
+	}
+	
+	public boolean getCorrect() {
+		return this.correct;
+	}
+	
+	public void setCorrect(boolean val) {
+		this.correct = val;
+	}
 }
